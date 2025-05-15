f0cc8f6bfd91d0145ade6b0d9d837a7d8f31e25c
Fabiano Junior Maia Manschein <16107182+Fabulani@users.noreply.github.com>
2025-05-15 13:16:55 +0300

    render as png

--- modify_ply.py
+++ modify_ply.py
@@ -1,10 +1,21 @@
 import open3d as o3d
 
+def render_as_png(pcd):
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(visible=False)
+    vis.add_geometry(pcd)
+    vis.update_geometry(pcd)
+    vis.poll_events()
+    vis.update_renderer()
+    vis.capture_screen_image('f{i}.png', do_render=True)
+    vis.destroy_window()
+
+
 ply_path = "../data/.fabiano/nerfstudio/garden_8/sparse_pc.ply"
 pcd = o3d.io.read_point_cloud(ply_path)
 
 print(pcd)
 print("Number of points:", len(pcd.points))
 
-o3d.visualization.draw_geometries([pcd])
+render_as_png(pcd)
 
