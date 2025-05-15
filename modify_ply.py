d4d17db77f6bb6eaf9a5553b45047847f35ec377
Fabiano Junior Maia Manschein <16107182+Fabulani@users.noreply.github.com>
2025-05-15 13:42:20 +0300

    Add function to rotate points

--- modify_ply.py
+++ modify_ply.py
@@ -3,36 +3,41 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+def rotate_point_cloud(points, angles_degrees):
+    """Rotate point cloud by given angles (in degrees) around x, y, z."""
+    angles = np.radians(angles_degrees)
+    Rx = np.array([[1, 0, 0],
+                   [0, np.cos(angles[0]), -np.sin(angles[0])],
+                   [0, np.sin(angles[0]),  np.cos(angles[0])]])
+    
+    Ry = np.array([[ np.cos(angles[1]), 0, np.sin(angles[1])],
+                   [0, 1, 0],
+                   [-np.sin(angles[1]), 0, np.cos(angles[1])]])
+    
+    Rz = np.array([[np.cos(angles[2]), -np.sin(angles[2]), 0],
+                   [np.sin(angles[2]),  np.cos(angles[2]), 0],
+                   [0, 0, 1]])
+    
+    R = Rz @ Ry @ Rx  
+    return points @ R.T
+
 def render_as_png(pcd):
-    # width, height = 1024, 768
-
-    # renderer = o3d.visualization.rendering.OffscreenRenderer(width, height)
-    # renderer.scene.set_background([1.0, 1.0, 1.0, 1.0])  # white
-
-    # material = o3d.visualization.rendering.MaterialRecord()
-    # material.shader = "defaultUnlit"
-
-    # renderer.scene.add_geometry("pcd", pcd, material)
-    # bounds = pcd.get_axis_aligned_bounding_box()
-    # center = bounds.get_center()
-    # extent = bounds.get_extent().max()
-
-    # renderer.setup_camera(60.0, bounds, center)
-    # img = renderer.render_to_image()
-    # o3d.io.write_image("render.png", img)
-
     points = np.asarray(pcd.points)
+
+    # rotate
+    points = rotate_point_cloud(points, angles_degrees=(45, 45, 45))
+
 
     # downsample
     if len(points) > 30000:
         pcd = pcd.farthest_point_down_sample(30000)
         points = np.asarray(pcd.points)
 
-    # 2D projection - ortho xy view
+    # 2D projection 
     plt.figure(figsize=(8, 6))
     plt.scatter(points[:, 0], points[:, 1], s=0.2, c=points[:, 2], cmap='viridis')
     plt.axis("equal")
-    plt.title("XY Projection of Point Cloud")
+    plt.title("Render of the point cloud")
     plt.savefig("render.png", dpi=300)
 
 
